3d474d0926a2d54bda18ca9a97717589c96530ed
Nitin K S <96114966+itsnitinks@users.noreply.github.com>
2022-06-08 21:19:52 +0530

    display of text

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -3,15 +3,24 @@ import styles from "./task.module.css";
 import Counter from "../Counter/Counter";
 const Task = ({ intotal }) => {
   console.log("props", intotal);
+
+  function changeit() {}
   // console.log(props.);
   // NOTE: do not delete `data-testid` key value pair
   return (
     <li data-testid="task" className={styles.task}>
       <input type="checkbox" data-testid="task-checkbox" />
-      <div data-testid="task-text">{}</div>
+      <div data-testid="task-text">{intotal.text}</div>
       <Counter />
       {/* Counter here */}
-      <button data-testid="task-remove-button">X</button>
+      <button
+        data-testid="task-remove-button"
+        onClick={() => {
+          changeit();
+        }}
+      >
+        X
+      </button>
     </li>
   );
 };
